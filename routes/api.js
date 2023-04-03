1cfbb937384e8c7c787dea540d041c6d3316afa6
LTKSK <ltksk0714@gmail.com>
2023-04-03 23:05:24 +0900

    add: cross-origin handson

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // プリフライトのときはX-Tokenのヘッダ送信を許可
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "X-Token");
+  }
+  next();
+});
+
 router.get("/", (req, res) => {
   res.send({ message: "Hello" });
 });
